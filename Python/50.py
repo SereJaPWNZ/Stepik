1110b6c78e0401d4e4e17fc04dc7781ec74b632b
SereJaPWNZ <oofatherxomgoo@gmail.com>
2022-07-27 21:34:05 +0300

    add 50

--- Python/50.py
+++ Python/50.py
@@ -6,3 +6,7 @@
 # Примечание. Шахматная ладья ходит по горизонтали или вертикали.
 
 x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
+if x1 == x2 or y1 == y2:
+    print('YES')
+else:
+    print('NO')
