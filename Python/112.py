dd640971cda08b308bd4b2bf3fe8c8a17804e630
SereJaPWNZ <oofatherxomgoo@gmail.com>
2022-09-05 22:36:58 +0300

    add 112

--- Python/112.py
+++ Python/112.py
@@ -5,3 +5,4 @@
 # Программа должна вывести произведение отличных от нуля чисел.
 # Примечание. Гарантируется, что хотя бы одно из 10 чисел является ненулевым.
 
+num1, num2, num3, num4, num5, num6, num7, num8, num9, num10 = int(input()), int(input()), int(input()), int(input()), int(input()), int(input()), int(input()), int(input()), int(input()), int(input())
