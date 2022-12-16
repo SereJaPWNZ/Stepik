d3eb2b82a28feac3539422b40548c2da10926ca5
Sergey Shevtsov <oofatherxomgoo@gmail.com>
2022-12-17 00:05:55 +0300

    update 34

--- 36.py
+++ 36.py
@@ -4,3 +4,12 @@
 # Формат выходных данных
 # Программа должна вывести «ДА», если соотношение выполняется, и «НЕТ» — если не выполняется.
 
+number = int(input())
+first_number = number // 1000
+second_number = number % 1000 // 100
+third_number = number % 100 // 10
+fourth_number = number % 10
+if (first_number+fourth_number) == (second_number - third_number):
+    print('ДА')
+else:
+    print('НЕТ')
