ecadb4cd6a6613f0471e3737bfd712fb311c2f73
Sergey Shevtsov <oofatherxomgoo@gmail.com>
2022-12-17 11:21:34 +0300

    update 40

--- 40.py
+++ 40.py
@@ -4,3 +4,16 @@
 # Формат выходных данных
 # Программа должна вывести наименьшее из четырёх чисел.
 
+a, b, c, d = int(input), int(input()), int(input()), int(input())
+if a > b:
+    ab = b
+else:
+    ab = a
+if c > d:
+    cd = d
+else:
+    cd = c
+if ab > cd:
+    print(cd)
+else:
+    print(ab)
