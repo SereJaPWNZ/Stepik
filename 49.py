b77e8ef58d0fe788bc0749cba47b234cc8438b2e
Sergey Shevtsov <oofatherxomgoo@gmail.com>
2022-12-17 20:47:10 +0300

    update 49

--- 49.py
+++ 49.py
@@ -7,3 +7,4 @@
 # Примечание. Шахматная ладья ходит по горизонтали или вертикали.
 
 x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
+print('YES' if x1 - x2 == 0 or y1 - y2 == 0 else 'NO')
