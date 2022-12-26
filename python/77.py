f849ef2a34072c9c7d8a77e4dd9bbcdee022d121
Sergey Shevtsov <oofatherxomgoo@gmail.com>
2022-12-26 23:38:48 +0300

    update 77

--- python/77.py
+++ python/77.py
@@ -6,3 +6,8 @@
 # Формат выходных данных
 # Программа должна вывести текст в соответствии с условием задачи.
 
+a, b, c = input()
+max_number = max(int(a), int(b), int(c))
+min_number = min(int(a), int(b), int(c))
+intermediate_number = (sum([int(a), int(b), int(c)]) - max_number - min_number)
+print('Число интересное' if max_number - min_number == intermediate_number else 'Число неинтересное')
