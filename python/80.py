ba9df6579871e8c3cd79f9ed0af7b922760ee3a9
Sergey Shevtsov <oofatherxomgoo@gmail.com>
2022-12-27 13:26:05 +0300

    update 80

--- python/80.py
+++ python/80.py
@@ -2,3 +2,4 @@
 # "Python is a great language!", said Fred. "I don't ever remember having this much fun before."
 # Примечание. Используйте конкатенацию строк.
 
+print('"Python is a great language!", said Fred. ' + '"I don'+"'t " + 'ever remember having this much fun before."')
