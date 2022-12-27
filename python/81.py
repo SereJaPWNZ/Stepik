20aed75397291d67d13be6bf5861ba8165e83471
Sergey Shevtsov <oofatherxomgoo@gmail.com>
2022-12-27 13:32:30 +0300

    update 81

--- python/81.py
+++ python/81.py
@@ -7,3 +7,11 @@
 # Формат выходных данных
 # Программа должна вывести текст в соответствии с условием задачи.
 # Примечание. Между firstname lastname вставьте пробел =)
+
+# var1
+firstname, lastname = input(), input()
+print(f'Hello', firstname, lastname + '! You just delved into Python')
+
+# var2
+firstname, lastname = input(), input()
+print(f'Hello {firstname} {lastname}! You just delved into Python')
