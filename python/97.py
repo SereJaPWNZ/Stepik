169ff9e2aa6fc423cfa6f929843373c85772044f
Sergey Shevtsov <oofatherxomgoo@gmail.com>
2023-01-06 21:54:12 +0300

    update 97

--- python/97.py
+++ python/97.py
@@ -5,3 +5,11 @@
 # Формат выходных данных
 # Программа должна вывести указанную последовательность символов.
 
+for _ in range(6):
+    print('AAA')
+for _ in range(5):
+    print('BBBB')
+print('E')
+for _ in range(9):
+    print('TTTTT')
+print('G')
