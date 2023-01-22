afe2cdfdcbc0e8ab15240c11f9cc46f7d96d26a0
Sergey Shevtsov <oofatherxomgoo@gmail.com>
2023-01-22 22:13:48 +0300

    update 117

--- python/117.py
+++ python/117.py
@@ -5,3 +5,15 @@
 # На вход программе подаются натуральное число n≥2, а затем n различных натуральных чисел, каждое на отдельной строке.
 # Формат выходных данных
 # Программа должна вывести два наибольших числа, каждое на отдельной строке.
+
+n, max_number, max_number_1 = int(input()), 1, 0
+for i in range(n):
+    temporary_number = int(input())
+    if max_number < temporary_number:
+        vrem = max_number
+        max_number = temporary_number
+        max_number_1 = vrem
+    else:
+        if max_number_1 < temporary_number:
+            max_number_1 = temporary_number
+print(max_number, max_number_1, sep='\n')
