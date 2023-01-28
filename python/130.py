d70967bb20fb255b51d18898699004f451fb7460
Sergey Shevtsov <oofatherxomgoo@gmail.com>
2023-01-28 21:12:56 +0300

    update 130

--- python/130.py
+++ python/130.py
@@ -12,3 +12,15 @@
 # Формат выходных данных
 # Программа должна вывести значения указанных величин в указанном порядке.
 
+n, sum_numbers, count_numbers, the_work_of_its_numbers = int(input()), 0, 0, 1
+dump_n= n
+while n != 0:
+    temp_number = n % 10
+    sum_numbers += temp_number
+    count_numbers += 1
+    the_work_of_its_numbers *= temp_number 
+    the_arithmetic_mean = sum_numbers / count_numbers
+    n //= 10
+first_number =  temp_number
+sum_first_and_last_digit = first_number + dump_n % 10
+print(sum_numbers, count_numbers, the_work_of_its_numbers, the_arithmetic_mean, first_number, sum_first_and_last_digit, sep = '\n')
