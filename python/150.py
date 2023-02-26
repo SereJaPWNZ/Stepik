5b1e6dabfe78fd571c4499a4666964d8ba5f4128
Sergey Shevtsov <oofatherxomgoo@gmail.com>
2023-02-26 12:39:37 +0300

    update 150

--- python/150.py
+++ python/150.py
@@ -14,3 +14,10 @@
 # Программа должна вывести треугольник в соответствии с условием.
 # Примечание. Используйте вложенный цикл for.
 
+n = int(input())
+counter = 0
+for i in range(n+1):
+    for j in range(i):
+        counter += 1
+        print(counter, end=' ')
+    print()
